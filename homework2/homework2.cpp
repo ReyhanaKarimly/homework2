2bcec4457dd28bc97a0a775aa845fc3536c4fb00
ReyhanaKarimly <karimlyreyhana@gmail.com>
2017-03-01 01:11:05 +0400

    Trapezium_square

--- homework2/homework2.cpp
+++ homework2/homework2.cpp
@@ -5,7 +5,7 @@ void main()
 	int a,b,h;
 	float S;
 	printf("If you want to calculate the square of trapezium");
-	printf("\nEnter a,b and h:");
+	printf("\nEnter a,b and h: ");
 scanf("%d%d%d",&a,&b,&h);
 S=((a+b)*h)/2.;
 printf("Square=%.3fm2",S);
