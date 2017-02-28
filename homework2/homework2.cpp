10810ea772443af50baf39a79d152d2b14ffb00e
ReyhanaKarimly <karimlyreyhana@gmail.com>
2017-02-28 23:35:45 +0400

    hw2

---
+++ homework2/homework2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h.>
+void main()
+{
+	printf("If you want to calculate the square of trapezium");
+	printf("\nEnter a,b and h:");
+	int a,b,h;
+float S;
+scanf("%d%d%d",&a,&b,&h);
+S=((a+b)*h)/2;
+printf("Square=%.3fm2",S);
+getch();
+}
+
